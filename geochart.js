ed05e1a9eaae113c08144b628856a1d9c23db3d6
Michael Zhu <mzhu23@gmail.com>
2020-03-26 01:26:09 +0000

    Added currency exchange form along with blue background for map

--- geochart.js
+++ geochart.js
@@ -7,15 +7,27 @@ google.charts.setOnLoadCallback(drawRegionsMap);
 function drawRegionsMap() {
   var data = google.visualization.arrayToDataTable([
     ['Country', 'Popularity'],
-    ['Germany', 200],
-    ['United States', 300],
-    ['Brazil', 400],
-    ['Canada', 500],
-    ['France', 600],
-    ['Russia', 700]
+    ['United States', 700],
+    ['Japan', 700],
+    ['United Kingdom', 700],
+    ['Australia', 700],
+    ['Canada', 700],
+    ['China', 700],
+    ['Russia', 700],
+    ['India', 700],
+    ['Brazil', 700],
+    ['Spain', 700],
+    ['France', 700],
+    ['Italy', 700],
+    ['Germany', 700],
+    ['Greece', 700],
+    ['Finland', 700],
+    ['Ireland', 700]
   ]);
 
-  var options = {};
+  var options = {
+    backgroundColor: '#81d4fa'
+  };
 
   var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
 
