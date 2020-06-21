9b0511bfb5ab2ada0ad2c26f5e54f2695628ec91
Michael Zhu <mzhu23@gmail.com>
2020-06-21 08:41:00 +0000

    Modified error function of AJAX call to display to user that internet connection is lost

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,11 +15,15 @@ function getExchangeRate(exchangeCurrencyCode){
     method: "GET",
     url: `https://openexchangerates.org/api/latest.json?app_id=d18484cad8964d32b4f71a7d9bd4005f&symbols=${exchangeCurrencyCode}`,
     success: function (data) {
+      exchangeAmount.classList.remove("badge-danger");
+      exchangeAmount.classList.add("badge-primary");
       exchangeRate = data.rates[exchangeCurrencyCode];
       exchangeAmount.textContent = exchangeRate.toFixed(4);
     },
     error: function(error){
-      console.log(error);
+      exchangeAmount.classList.remove("badge-primary");
+      exchangeAmount.classList.add("badge-danger");
+      exchangeAmount.textContent = "Internet connection lost. Please try again.";
     }
   })
 }
